84fd4a59906ac09df88ab1f433832d5373e22fe0
Paul Dijou <paul.dijou@gmail.com>
2015-04-25 19:27:50 +0200

    Remove useless import

--- scala-jwt-play-json/src/main/scala/package.scala
+++ scala-jwt-play-json/src/main/scala/package.scala
@@ -1,6 +1,3 @@
 package pdi.scala
 
-import play.api.libs.json._
-import play.api.libs.functional.syntax._
-
 package object jwt extends JwtJsonImplicits {}
--- scala-jwt-play/src/main/scala/package.scala
+++ scala-jwt-play/src/main/scala/package.scala
@@ -1,7 +1,3 @@
 package pdi.scala
 
-import play.api.Play
-import play.api.mvc.{Result, RequestHeader}
-import play.api.libs.json.JsObject
-
 package object jwt extends JwtJsonImplicits with JwtPlayImplicits {}
