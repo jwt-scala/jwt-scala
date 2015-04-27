87599ecd5fe101ecb93d2c022ae4b6c9d2555a87
Paul Dijou <paul.dijou@gmail.com>
2015-04-27 18:49:29 +0200

    Fix typo

--- scala-jwt-core/src/main/scala-java-legacy/JwtBase64Impl.scala
+++ scala-jwt-core/src/main/scala-java-legacy/JwtBase64Impl.scala
@@ -5,6 +5,6 @@ import org.apache.commons.codec.binary.Base64
 trait JwtBase64Impl {
   val codec = new Base64(true)
 
-  def encode(value: Array[Byte]): Array[Byte] = codec.encode(value))
+  def encode(value: Array[Byte]): Array[Byte] = codec.encode(value)
   def decode(value: Array[Byte]): Array[Byte] = codec.decode(value)
 }
