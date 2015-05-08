55f028971cf415feed88c0f410e9ad2a6bf079f1
Paul Dijou <paul.dijou@gmail.com>
2015-05-08 23:01:59 +0200

    Update publish script

--- publi.sh
+++ publi.sh
@@ -21,6 +21,7 @@ sed -i "s/buildVersion = \"$OLDVERSION\"/buildVersion = \"$VERSION\"/g" ./projec
 sed -i "s/version := \"$OLDVERSION\"/version := \"$VERSION\"/g" ./examples/play-angular-standalone/build.sbt
 sed -i "s/\"pdi\" %% \"jwt-play\" % \"$OLDVERSION\"/\"pdi\" %% \"jwt-play\" % \"$VERSION\"/g" ./examples/play-angular-standalone/build.sbt
 sed -i "s/version: $OLDVERSION/version: $VERSION/g" ../jwt-scala-doc/_config.yml
+sed -i "s/versions: [/versions: [$OLDVERSION, /g" ../jwt-scala-doc/_config.yml
 
 echo "Generating ScalaDoc"
 sbt doc
@@ -31,6 +32,9 @@ mkdir ../jwt-scala-doc/api/$VERSION
 cp -r ./core/common/target/edge/scala-2.11/api ../jwt-scala-doc/api/$VERSION/jwt-core
 cp -r ./play-json/target/edge/scala-2.11/api ../jwt-scala-doc/api/$VERSION/jwt-play-json
 cp -r ./play/target/edge/scala-2.11/api ../jwt-scala-doc/api/$VERSION/jwt-play
+cp ../jwt-scala-doc/api/$OLDVERSION/index.html ../jwt-scala-doc/api/$VERSION/index.html
+
+sed -i "s/$OLDVERSION/$VERSION/g" ../jwt-scala-doc/api/$VERSION/index.html
 
 echo "Pushing to GitHub"
 git add .
