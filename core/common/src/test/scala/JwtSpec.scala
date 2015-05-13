2aa8bc6d0cbd1a565ad4c5afa641e9bb88aed068
Paul Dijou <paul.dijou@gmail.com>
2015-05-13 20:47:04 +0200

    Fix tests by mocking time

--- core/common/src/test/scala/JwtSpec.scala
+++ core/common/src/test/scala/JwtSpec.scala
@@ -7,58 +7,76 @@ import scala.util.{Success, Failure}
 class JwtSpec extends UnitSpec with Fixture {
   describe("Jwt") {
     it("should encode Hmac") {
+      val mock = mockBeforeExpiration
       data foreach { d =>
         assertResult(d.token, d.algo.fullName) { Jwt.encode(d.header, claim, secretKey.get, d.algo) }
       }
+      mock.tearDown
     }
 
     it("should encode RSA") {
+      val mock = mockBeforeExpiration
       dataRSA foreach { d =>
         assertResult(d.token, d.algo.fullName) { Jwt.encode(d.header, claim, privateKeyRSA.get, d.algo) }
       }
+      mock.tearDown
     }
 
     it("should be symetric") {
+      val mock = mockBeforeExpiration
       data foreach { d =>
         assertResult((d.header, claim, Option(d.signature)), d.algo.fullName) {
           Jwt.decodeAll(Jwt.encode(d.header, claim, secretKey.get, d.algo), secretKey).get
         }
       }
+      mock.tearDown
     }
 
     it("should encode case class") {
+      val mock = mockBeforeExpiration
       data foreach { d =>
         assertResult(d.token, d.algo.fullName) { Jwt.encode(d.headerClass, claimClass, secretKey.get) }
       }
+      mock.tearDown
     }
 
     it("should decodeRawAll") {
+      val mock = mockBeforeExpiration
       data foreach { d =>
         assertResult(Success((d.header, claim, Some(d.signature))), d.algo.fullName) { Jwt.decodeRawAll(d.token, secretKey) }
       }
+      mock.tearDown
     }
 
     it("should decodeRaw") {
+      val mock = mockBeforeExpiration
       data foreach { d =>
         assertResult(Success((claim)), d.algo.fullName) { Jwt.decodeRaw(d.token, secretKey) }
       }
+      mock.tearDown
     }
 
     it("should decodeAll") {
+      val mock = mockBeforeExpiration
       data foreach { d =>
         assertResult(Success((d.header, claim, Some(d.signature))), d.algo.fullName) { Jwt.decodeAll(d.token, secretKey) }
       }
+      mock.tearDown
     }
 
     it("should decode") {
+      val mock = mockBeforeExpiration
       data foreach { d =>
         assertResult(Success(claim), d.algo.fullName) { Jwt.decode(d.token, secretKey) }
       }
+      mock.tearDown
     }
 
     it("should validate") {
+      val mock = mockBeforeExpiration
       data foreach { d => assertResult((), d.algo.fullName) { Jwt.validate(d.token, secretKey) } }
       dataRSA foreach { d => assertResult((), d.algo.fullName) { Jwt.validate(d.token, publicKeyRSA) } }
+      mock.tearDown
     }
   }
 }
