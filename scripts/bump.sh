bc88ef105df17a6d9c07f7aa938c589c2548a380
Paul Dijou <paul.dijou@gmail.com>
2015-05-14 11:32:43 +0200

    Fix scripts

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -10,7 +10,7 @@ sed -i "s/$OLDVERSION/$VERSION/g" ./README.md
 sed -i "s/version := \"$OLDVERSION\"/version := \"$VERSION\"/g" ./examples/play-angular-standalone/build.sbt
 sed -i "s/\"pdi\" %% \"jwt-play\" % \"$OLDVERSION\"/\"pdi\" %% \"jwt-play\" % \"$VERSION\"/g" ./examples/play-angular-standalone/build.sbt
 sed -i "s/version: $OLDVERSION/version: $VERSION/g" ./docs/src/site/_config.yml
-sed -i "s/versions: [/versions: [$OLDVERSION, /g" ./docs/src/site/_config.yml
+sed -i "s/versions: \[/versions: \[$OLDVERSION, /g" ./docs/src/site/_config.yml
 
 # The end
 exit 0;
