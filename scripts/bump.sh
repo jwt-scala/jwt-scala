00d8fe590ed69fc3743a75b433ff48ee71f34af8
Paul Dijou <paul.dijou@gmail.com>
2015-12-31 16:24:00 +0100

    Fix sed args on OSX

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -6,11 +6,11 @@ VERSION=$2
 echo "Stating publish process for JWT Scala from $OLDVERSION to $VERSION ..."
 
 echo "Upgrading version in files"
-sed -i "s/$OLDVERSION/$VERSION/g" ./README.md
-sed -i "s/version := \"$OLDVERSION\"/version := \"$VERSION\"/g" ./examples/play-angular-standalone/build.sbt
-sed -i "s/\"pdi\" %% \"jwt-play\" % \"$OLDVERSION\"/\"pdi\" %% \"jwt-play\" % \"$VERSION\"/g" ./examples/play-angular-standalone/build.sbt
-sed -i "s/version: $OLDVERSION/version: $VERSION/g" ./docs/src/site/_config.yml
-sed -i "s/versions: \[/versions: \[$OLDVERSION, /g" ./docs/src/site/_config.yml
+sed -i '' -e "s/$OLDVERSION/$VERSION/g" ./README.md
+sed -i '' -e "s/version := \"$OLDVERSION\"/version := \"$VERSION\"/g" ./examples/play-angular-standalone/build.sbt
+sed -i '' -e "s/\"pdi\" %% \"jwt-play\" % \"$OLDVERSION\"/\"pdi\" %% \"jwt-play\" % \"$VERSION\"/g" ./examples/play-angular-standalone/build.sbt
+sed -i '' -e "s/version: $OLDVERSION/version: $VERSION/g" ./docs/src/site/_config.yml
+sed -i '' -e "s/versions: \[/versions: \[$OLDVERSION, /g" ./docs/src/site/_config.yml
 
 # The end
 exit 0;
