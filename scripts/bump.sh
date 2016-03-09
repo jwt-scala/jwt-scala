e385a763fdb9576f22670e2bf3c68ef16657c08b
Paul Dijou <paul.dijou@gmail.com>
2016-03-09 14:46:06 +0100

    Fix bump script

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -8,7 +8,7 @@ echo "Stating publish process for JWT Scala from $OLDVERSION to $VERSION ..."
 echo "Upgrading version in files"
 sed -i '' -e "s/$OLDVERSION/$VERSION/g" ./README.md
 sed -i '' -e "s/version := \"$OLDVERSION\"/version := \"$VERSION\"/g" ./examples/play-angular-standalone/build.sbt
-sed -i '' -e "s/\"pdi\" %% \"jwt-play\" % \"$OLDVERSION\"/\"pdi\" %% \"jwt-play\" % \"$VERSION\"/g" ./examples/play-angular-standalone/build.sbt
+sed -i '' -e "s/\"com.pauldijou\" %% \"jwt-play\" % \"$OLDVERSION\"/\"com.pauldijou\" %% \"jwt-play\" % \"$VERSION\"/g" ./examples/play-angular-standalone/build.sbt
 sed -i '' -e "s/version: $OLDVERSION/version: $VERSION/g" ./docs/src/site/_config.yml
 sed -i '' -e "s/versions: \[/versions: \[$OLDVERSION, /g" ./docs/src/site/_config.yml
 
