2e1e3d57f081c0f993f4d8116bccc1f5f19af4e9
Paul Dijou <paul.dijou@gmail.com>
2017-01-14 17:57:24 +0100

    Fix doc generation

--- scripts/scaladoc.sh
+++ scripts/scaladoc.sh
@@ -16,18 +16,18 @@ mkdir ./docs/src/site/api/latest
 mkdir ./docs/src/site/api/$VERSION
 
 # Copy scaladoc
-cp -r ./core/common/target/edge/scala-2.11/api ./docs/src/site/api/latest/jwt-core
-cp -r ./json/play-json/target/edge/scala-2.11/api ./docs/src/site/api/latest/jwt-play-json
-cp -r ./json/json4s-native/target/edge/scala-2.11/api ./docs/src/site/api/latest/jwt-json4s
-cp -r ./json/circe/target/edge/scala-2.11/api ./docs/src/site/api/latest/jwt-circe
-cp -r ./play/target/edge/scala-2.11/api ./docs/src/site/api/latest/jwt-play
+cp -r ./core/common/target/edge/scala-2.12/api ./docs/src/site/api/latest/jwt-core
+cp -r ./json/play-json/target/edge/scala-2.12/api ./docs/src/site/api/latest/jwt-play-json
+cp -r ./json/json4s-native/target/edge/scala-2.12/api ./docs/src/site/api/latest/jwt-json4s
+cp -r ./json/circe/target/edge/scala-2.12/api ./docs/src/site/api/latest/jwt-circe
+cp -r ./json/upickle/target/edge/scala-2.12/api ./docs/src/site/api/latest/jwt-upickle
 cp ./docs/src/site/_templates/index_api.html ./docs/src/site/api/latest/index.html
 sed -i.tmp "s/placeholderVersion/$VERSION/g" ./docs/src/site/api/latest/index.html
 
-cp -r ./core/common/target/edge/scala-2.11/api ./docs/src/site/api/$VERSION/jwt-core
-cp -r ./json/play-json/target/edge/scala-2.11/api ./docs/src/site/api/$VERSION/jwt-play-json
-cp -r ./json/json4s-native/target/edge/scala-2.11/api ./docs/src/site/api/$VERSION/jwt-json4s
-cp -r ./json/circe/target/edge/scala-2.11/api ./docs/src/site/api/$VERSION/jwt-circe
-cp -r ./play/target/edge/scala-2.11/api ./docs/src/site/api/$VERSION/jwt-play
+cp -r ./core/common/target/edge/scala-2.12/api ./docs/src/site/api/$VERSION/jwt-core
+cp -r ./json/play-json/target/edge/scala-2.12/api ./docs/src/site/api/$VERSION/jwt-play-json
+cp -r ./json/json4s-native/target/edge/scala-2.12/api ./docs/src/site/api/$VERSION/jwt-json4s
+cp -r ./json/circe/target/edge/scala-2.12/api ./docs/src/site/api/$VERSION/jwt-circe
+cp -r ./json/upickle/target/edge/scala-2.12/api ./docs/src/site/api/$VERSION/jwt-upickle
 cp ./docs/src/site/_templates/index_api.html ./docs/src/site/api/$VERSION/index.html
 sed -i.tmp "s/placeholderVersion/$VERSION/g" ./docs/src/site/api/$VERSION/index.html
