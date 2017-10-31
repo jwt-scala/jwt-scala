05f9794b874a14befe252ebae6c15778c05871f9
Paul Dijou <paul.dijou@gmail.com>
2017-10-31 17:44:24 +0100

    Update examples to Play 2.6.6

--- examples/play-angular-standalone/app/controllers/Secured.scala
+++ examples/play-angular-standalone/app/controllers/Secured.scala
@@ -1,36 +1,56 @@
 package controllers
 
-import scala.concurrent.Future
-import play.api.libs.concurrent.Execution.Implicits._
-
-import play.api.mvc._
-import play.api.mvc.Results._
-import play.api.libs.json._
-
-import pdi.jwt._
+import javax.inject.Inject
 
 import models.User
+import pdi.jwt.JwtSession._
+import play.api.http.FileMimeTypes
+import play.api.i18n.{ Langs, MessagesApi }
+import play.api.mvc.Results._
+import play.api.mvc._
+
+import scala.concurrent.{ ExecutionContext, Future }
 
 class AuthenticatedRequest[A](val user: User, request: Request[A]) extends WrappedRequest[A](request)
 
-trait Secured {
-  def Authenticated = AuthenticatedAction
-  def Admin = AdminAction
+class AuthenticatedActionBuilder @Inject()(parser: BodyParsers.Default)(implicit ec: ExecutionContext)
+    extends ActionBuilderImpl(parser) {
+  override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]): Future[Result] = {
+    request.jwtSession.getAs[User]("user") match {
+      case Some(user) =>
+        block(new AuthenticatedRequest[A](user, request)).map(_.refreshJwtSession(request))
+      case _ =>
+        Future(Unauthorized)
+    }
+  }
 }
 
-object AuthenticatedAction extends ActionBuilder[AuthenticatedRequest] {
-  def invokeBlock[A](request: Request[A], block: AuthenticatedRequest[A] => Future[Result]) =
+class AdminActionBuilder @Inject()(parser: BodyParsers.Default)(implicit ec: ExecutionContext)
+    extends ActionBuilderImpl(parser) {
+  override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]): Future[Result] = {
     request.jwtSession.getAs[User]("user") match {
-      case Some(user) => block(new AuthenticatedRequest(user, request)).map(_.refreshJwtSession(request))
-      case _ => Future.successful(Unauthorized)
+      case Some(user) if user.isAdmin =>
+        block(new AuthenticatedRequest(user, request)).map(_.refreshJwtSession(request))
+      case Some(_) =>
+        Future(Forbidden.refreshJwtSession(request))
+      case _ =>
+        Future(Unauthorized)
     }
+  }
 }
 
-object AdminAction extends ActionBuilder[AuthenticatedRequest] {
-  def invokeBlock[A](request: Request[A], block: AuthenticatedRequest[A] => Future[Result]) =
-    request.jwtSession.getAs[User]("user") match {
-      case Some(user) if user.isAdmin => block(new AuthenticatedRequest(user, request)).map(_.refreshJwtSession(request))
-      case Some(_) => Future.successful(Forbidden.refreshJwtSession(request))
-      case _ => Future.successful(Unauthorized)
-    }
+case class SecuredControllerComponents @Inject()(
+    adminActionBuilder: AdminActionBuilder,
+    authenticatedActionBuilder: AuthenticatedActionBuilder,
+    actionBuilder: DefaultActionBuilder,
+    parsers: PlayBodyParsers,
+    messagesApi: MessagesApi,
+    langs: Langs,
+    fileMimeTypes: FileMimeTypes,
+    executionContext: scala.concurrent.ExecutionContext
+) extends ControllerComponents
+
+class SecuredController @Inject()(scc: SecuredControllerComponents) extends AbstractController(scc) {
+  def AdminAction: AdminActionBuilder                 = scc.adminActionBuilder
+  def AuthenticatedAction: AuthenticatedActionBuilder = scc.authenticatedActionBuilder
 }
--- examples/play-angular-standalone/build.sbt
+++ examples/play-angular-standalone/build.sbt
@@ -4,10 +4,11 @@ version := "0.14.1"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.11.6"
+scalaVersion := "2.12.4"
 
-routesGenerator := InjectedRoutesGenerator
+routesGenerator := play.sbt.routes.RoutesKeys.InjectedRoutesGenerator
 
 libraryDependencies ++= Seq(
-  "com.pauldijou" %% "jwt-play" % "0.14.1"
+  guice,
+  "com.pauldijou"     %% "jwt-play" % "0.14.1"
 )
