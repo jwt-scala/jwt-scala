9af4531339e40f6d770fd5746c199faa1e2afbab
Paul Dijou <paul.dijou@gmail.com>
2018-03-05 21:37:39 +0000

    Remove old doc

--- docs/src/site/api/0.0.5/jwt-play-json/index.js
+++
@@ -1,1 +0,0 @@
-Index.PACKAGES = {"pdi" : [], "pdi.jwt" : [{"object" : "pdi\/jwt\/JwtJson$.html", "name" : "pdi.jwt.JwtJson"}, {"trait" : "pdi\/jwt\/JwtJsonImplicits.html", "name" : "pdi.jwt.JwtJsonImplicits"}]};
--- docs/src/site/api/0.9.1/jwt-circe/index.js
+++
@@ -1,1 +0,0 @@
-Index.PACKAGES = {"pdi" : [], "pdi.jwt" : [{"object" : "pdi\/jwt\/JwtCirce$.html", "name" : "pdi.jwt.JwtCirce"}]};
