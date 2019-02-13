fd3b245ac9efa44ff65d59d5967761fd658af9e5
Peter Rakhunov <prakhunov@gmail.com>
2019-02-12 18:26:12 -0600

    updated built in sample project to work with play 2.7

--- examples/play-angular/app/controllers/Secured.scala
+++ examples/play-angular/app/controllers/Secured.scala
@@ -1,19 +1,19 @@
 package controllers
 
 import javax.inject.Inject
-
 import models.User
 import pdi.jwt.JwtSession._
+import play.api.Configuration
 import play.api.http.FileMimeTypes
-import play.api.i18n.{ Langs, MessagesApi }
+import play.api.i18n.{Langs, MessagesApi}
 import play.api.mvc.Results._
 import play.api.mvc._
 
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.{ExecutionContext, Future}
 
 class AuthenticatedRequest[A](val user: User, request: Request[A]) extends WrappedRequest[A](request)
 
-class AuthenticatedActionBuilder @Inject()(parser: BodyParsers.Default)(implicit ec: ExecutionContext)
+class AuthenticatedActionBuilder @Inject()(parser: BodyParsers.Default)(implicit ec: ExecutionContext, conf:Configuration)
     extends ActionBuilderImpl(parser) {
   override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]): Future[Result] = {
     request.jwtSession.getAs[User]("user") match {
@@ -25,7 +25,7 @@ class AuthenticatedActionBuilder @Inject()(parser: BodyParsers.Default)(implicit
   }
 }
 
-class AdminActionBuilder @Inject()(parser: BodyParsers.Default)(implicit ec: ExecutionContext)
+class AdminActionBuilder @Inject()(parser: BodyParsers.Default)(implicit ec: ExecutionContext, conf:Configuration)
     extends ActionBuilderImpl(parser) {
   override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]): Future[Result] = {
     request.jwtSession.getAs[User]("user") match {
