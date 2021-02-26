980a8c0c7b8051205829af28a0bf5fadcb5262b1
Erwan Loisant <erwan@loisant.com>
2021-02-26 21:17:18 +0100

    Add json4sNativeProject to CI

--- json/json4s-native/src/test/scala/Json4sNativeSpec.scala
+++ json/json4s-native/src/test/scala/Json4sNativeSpec.scala
@@ -11,35 +11,57 @@ class JwtJson4sNativeSpec extends JwtJsonCommonSpec[JObject] with Json4sNativeFi
 
   describe("JwtJson") {
     it("should implicitly convert to JValue") {
-      assertResult((
-        ("iss" -> "me") ~
-        ("aud" -> Option("you")) ~
-        ("sub" -> "something") ~
-        ("exp" -> 15) ~
-        ("nbf" -> 10) ~
-        ("iat" -> 10)
-      ), "Claim") {
-        JwtClaim().by("me").to("you").about("something").issuedAt(10).startsAt(10).expiresAt(15).toJValue
+      assertResult(
+        (
+          ("iss" -> "me") ~
+            ("aud" -> Option("you")) ~
+            ("sub" -> "something") ~
+            ("exp" -> 15) ~
+            ("nbf" -> 10) ~
+            ("iat" -> 10)
+        ),
+        "Claim"
+      ) {
+        JwtClaim()
+          .by("me")
+          .to("you")
+          .about("something")
+          .issuedAt(10)
+          .startsAt(10)
+          .expiresAt(15)
+          .toJValue()
       }
 
-      assertResult((
-        ("typ" -> "JWT") ~
-        ("alg" -> "HS256")
-      ), "Claim") {
-        JwtHeader(JwtAlgorithm.HS256).toJValue
+      assertResult(
+        (
+          ("typ" -> "JWT") ~
+            ("alg" -> "HS256")
+        ),
+        "Claim"
+      ) {
+        JwtHeader(JwtAlgorithm.HS256).toJValue()
       }
     }
 
     it("should implicitly convert to JValue when audience is many") {
-      assertResult((
-        ("iss" -> "me") ~
-        ("aud" -> Set("you", "another")) ~
-        ("sub" -> "something") ~
-        ("exp" -> 15) ~
-        ("nbf" -> 10) ~
-        ("iat" -> 10)
-      ), "Claim") {
-        JwtClaim(audience = Some(Set("you", "another"))).by("me").about("something").issuedAt(10).startsAt(10).expiresAt(15).toJValue
+      assertResult(
+        (
+          ("iss" -> "me") ~
+            ("aud" -> Set("you", "another")) ~
+            ("sub" -> "something") ~
+            ("exp" -> 15) ~
+            ("nbf" -> 10) ~
+            ("iat" -> 10)
+        ),
+        "Claim"
+      ) {
+        JwtClaim(audience = Some(Set("you", "another")))
+          .by("me")
+          .about("something")
+          .issuedAt(10)
+          .startsAt(10)
+          .expiresAt(15)
+          .toJValue()
       }
     }
   }
