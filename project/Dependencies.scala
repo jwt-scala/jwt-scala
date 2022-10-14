514da824b3a6955bf9e95e16916fbfce78ce1521
Scala Steward <scala_steward@virtuslab.com>
2022-10-14 23:41:03 +0000

    Update json4s-core, json4s-jackson-core, ... to 4.0.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
 
     val play = "2.8.16"
     val playJson = "2.9.3"
-    val json4s = "4.0.5"
+    val json4s = "4.0.6"
     val circe = "0.14.2"
     val upickle = "2.0.0"
     val zioJson = "0.3.0-RC11"
