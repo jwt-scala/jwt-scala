c1874e48f0dd84d627cf6e715306d30a1a821caf
Scala Steward <scala_steward@virtuslab.com>
2023-04-13 16:09:09 +0000

    Update bcpkix-jdk18on to 1.73

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object V {
     val munit = "0.7.29"
-    val bouncyCastle = "1.72"
+    val bouncyCastle = "1.73"
     val guice = "4.2.3"
 
     val play = "2.8.19"
