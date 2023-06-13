129fc823cab7ed52864827f57a483ffcd88f0dad
Ondra Pelech <ondra.pelech@gmail.com>
2023-06-13 13:10:58 +0200

    Fix

--- core/shared/src/main/scala/JwtUtils.scala
+++ core/shared/src/main/scala/JwtUtils.scala
@@ -307,8 +307,8 @@ object JwtUtils {
       signatureDER += 0x81.toByte
 
     signatureDER += signatureLength.toByte
-    val _ = signatureDER += 2.toByte += r.length.toByte ++= r
-    val _ = signatureDER += 2.toByte += s.length.toByte ++= s
+    signatureDER += 2.toByte += r.length.toByte ++= r
+    signatureDER += 2.toByte += s.length.toByte ++= s
 
     signatureDER.toArray
   }
