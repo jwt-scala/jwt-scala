ff2f5d145543fffdede049894357f9aaf5922ec1
Scala Steward <scala_steward@virtuslab.com>
2023-06-18 14:59:43 +0000

    Revert commit(s) 68b7beb

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object Dependencies {
   object V {
     val munit = "0.7.29"
-    val bouncyCastle = "1.74"
+    val bouncyCastle = "1.73"
     val guice = "4.2.3"
 
     val scalaJavaTime = "2.5.0"
