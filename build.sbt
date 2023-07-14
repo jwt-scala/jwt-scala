d06e59c9da11923085a44da97756d0e5aa67daa5
Erwan Loisant <erwan@loisant.com>
2023-07-14 23:15:49 +0200

    Simplifiy Deps file

--- build.sbt
+++ build.sbt
@@ -1,7 +1,6 @@
 import scala.io.Source
 import scala.sys.process._
 
-import Dependencies._
 import com.jsuereth.sbtpgp.PgpKeys._
 import microsites._
 import sbt.Keys._
@@ -12,7 +11,7 @@ val previousVersion = "9.4.0"
 val buildVersion = "9.4.1"
 
 val scala212 = "2.12.15"
-val scala213 = "2.13.11"
+val scala213 = "2.13.8"
 val scala3 = "3.3.0"
 
 Global / onChangedBuildSource := ReloadOnSourceChanges
---
+++ project/Libs.scala
@@ -0,0 +1,66 @@
+import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
+import sbt._
+
+object Versions {
+  val munit = "1.0.0-M8"
+
+  val bouncyCastle = "1.75"
+
+  val guice = "4.2.3"
+
+  val scalaJavaTime = "2.5.0"
+
+  val scalajsSecureRandom = "1.0.0"
+
+  val play = "2.8.19"
+
+  val playJson = "2.9.4"
+
+  val json4s = "4.0.6"
+
+  val circe = "0.14.5"
+
+  val upickle = "3.1.0"
+
+  val zioJson = "0.6.0"
+
+  val argonaut = "6.3.8"
+}
+
+object Libs {
+  val scalaJavaTime =
+    Def.setting("io.github.cquiroz" %%% "scala-java-time" % Versions.scalaJavaTime)
+  val scalajsSecureRandom = Def.setting(
+    ("org.scala-js" %%% "scalajs-java-securerandom" % Versions.scalajsSecureRandom).cross(
+      CrossVersion.for3Use2_13
+    )
+  )
+
+  val play = "com.typesafe.play" %% "play" % Versions.play
+  val playJson = "com.typesafe.play" %% "play-json" % Versions.playJson
+  val playTest = "com.typesafe.play" %% "play-test" % Versions.play % Test
+  val playTestProvided = "com.typesafe.play" %% "play-test" % Versions.play
+  val guice = "com.google.inject" % "guice" % Versions.guice % "test"
+
+  val json4sCore = "org.json4s" %% "json4s-core" % Versions.json4s
+  val json4sNative = "org.json4s" %% "json4s-native-core" % Versions.json4s
+  val json4sJackson = "org.json4s" %% "json4s-jackson-core" % Versions.json4s
+
+  val circeCore = Def.setting("io.circe" %%% "circe-core" % Versions.circe)
+  val circeGeneric = Def.setting("io.circe" %%% "circe-generic" % Versions.circe)
+  val circeJawn = Def.setting("io.circe" %%% "circe-jawn" % Versions.circe)
+  val circeParse = Def.setting("io.circe" %%% "circe-parser" % Versions.circe)
+
+  val upickle = "com.lihaoyi" %% "upickle" % Versions.upickle
+
+  val zioJson = "dev.zio" %% "zio-json" % Versions.zioJson
+
+  val argonaut = "io.argonaut" %% "argonaut" % Versions.argonaut
+
+  val bouncyCastle = "org.bouncycastle" % "bcpkix-jdk18on" % Versions.bouncyCastle % Test
+  val bouncyCastleTut = "org.bouncycastle" % "bcpkix-jdk18on" % Versions.bouncyCastle
+
+  val munit = Def.setting("org.scalameta" %%% "munit" % Versions.munit % Test)
+  val munitScalacheck =
+    Def.setting("org.scalameta" %%% "munit-scalacheck" % Versions.munit % Test)
+}
