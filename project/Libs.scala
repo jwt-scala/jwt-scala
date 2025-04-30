6c31067d1da6f1011a598a6213f4a8e13d09d38e
Scala Steward <scala_steward@virtuslab.com>
2025-04-30 16:25:44 +0000

    Update munit to 1.1.1

--- project/Libs.scala
+++ project/Libs.scala
@@ -2,7 +2,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 import sbt._
 
 object Versions {
-  val munit = "1.1.0"
+  val munit = "1.1.1"
 
   val bouncyCastle = "1.80"
 
